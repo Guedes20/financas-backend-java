374480c763ef0b7bfd659fb40c53e6a3d07d3a97
guedes20 <fernandoguedes20@gmail.com>
2020-01-26 20:12:19 -0300

    Implementando o metodo de buscar da classe LancamentoController.

--- src/main/java/br/com/minhasfinancas/controller/LancamentoController.java
+++ src/main/java/br/com/minhasfinancas/controller/LancamentoController.java
@@ -1,14 +1,20 @@
 package br.com.minhasfinancas.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.catalina.connector.Response;
+import org.springframework.expression.spel.ast.OpInc;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.minhasfinancas.dto.LancamentoDTO;
@@ -58,13 +64,37 @@ public class LancamentoController {
 	}
 
 	@DeleteMapping
-	 public ResponseEntity atualizar(@PathVariable("id") Long id) {
-		return lancamentoService.obterPorId(id).map(entidade ->{
-             lancamentoService.deletar(entidade);
-             return new ResponseEntity(HttpStatus.NO_CONTENT);
- 		}).orElseGet(() -> new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
+	public ResponseEntity atualizar(@PathVariable("id") Long id) {
+		return lancamentoService.obterPorId(id).map(entidade -> {
+			lancamentoService.deletar(entidade);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}).orElseGet(() -> new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
 	}
-	
+
+	@GetMapping
+	public ResponseEntity buscar(@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano, 
+			@RequestParam("usuario") Long idUsuario) {
+
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+
+		Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);
+
+		if (usuario.isPresent()) {
+			return ResponseEntity.badRequest()
+					.body("Não foi possivel realizar a consulta.Usuário não encontrado para o id informado.");
+		} else {
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+
+		List<Lancamento> lancamentos = lancamentoService.buscar(lancamentoFiltro);
+		return ResponseEntity.ok(lancamentos);
+	}
+
 	private Lancamento converter(LancamentoDTO dto) {
 		Lancamento l = new Lancamento();
 		l.setAno(dto.getAno());
